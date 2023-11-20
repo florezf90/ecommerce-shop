3f09c13d1d35580afb1874c25251a684eac8d8dd
florezf90 <florezf90@gmail.com>
2023-11-20 01:01:51 -0600

    completed the creation for the remaining models needed and also added the mandatory code to complete thge rout handlers to modify the database depending of the url path

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
